1ae7e59557ca6de015f1ca0636ee0e551b8698ec
BertJan Bakker <bertjanbakker@gmail.com>
2018-06-28 23:59:05 +0200

    Added a program that reads user input

---
+++ user_input.py
@@ -0,0 +1,7 @@
+print("hello")
+name = input("What is your name? ")
+print("Hello " + name)
+
+number = input("What is your favorite number? ")
+triple = int(number) * 3
+print("Your number times three is " + str(triple))

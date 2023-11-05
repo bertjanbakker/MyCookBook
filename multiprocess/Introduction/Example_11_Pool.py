16f14bccefa148c2d5d2c8878d40966216fc6b0d
BertJan Bakker <bakker.bj@tkppensioen.nl>
2023-11-05 13:43:48 +0100

    some logging

--- multiprocess/Introduction/Example_11_Pool.py
+++ multiprocess/Introduction/Example_11_Pool.py
@@ -5,6 +5,9 @@
 from multiprocessing import Pool, TimeoutError
 import time
 import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
 def f(x):
@@ -14,7 +17,7 @@ def f(x):
 if __name__ == "__main__":
     # start 4 worker processes
     with Pool(processes=4) as pool:
-        # print "[0, 1, 4,..., 81]"
+        #logging.info("[0, 1, 4,..., 81]")
         print(pool.map(f, range(10)))
 
         # print same numbers in arbitrary order
